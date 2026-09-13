9e998b5da57431610f553295773c20635bc54a8f
Rumi Rifai <112601876+rumirifai@users.noreply.github.com>
2026-09-14 02:00:00 +0700

    Fix deployment locale config

--- astro.config.ts
+++ astro.config.ts
@@ -29,8 +29,8 @@ export default defineConfig({
     }),
   ],
   i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
+    locales: ["id"],
+    defaultLocale: "id",
     routing: {
       prefixDefaultLocale: false,
     },
